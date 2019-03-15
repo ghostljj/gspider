e7e0ad16f2a1fd401497b3146f611d0be7876433
ghostljj <ghostljj@gmail.com>
2019-03-16 00:05:31 +0800

    uu

--- gspider_printinfo.go
+++ gspider_printinfo.go
@@ -8,40 +8,40 @@ import (
 //打印 请求 头信息 查看信息用
 func (s *Spider) PrintReqHeader(key string) {
 	if key == "" {
-		fmt.Println("------------------  S Req 请求 Header | GetReqHeader() map[string][]string")
+		fmt.Println("------------------  Req 请求 Header | GetReqHeader() map[string][]string")
 		fmt.Println("------------------  使用 GetReqHeader() http.Header 可以使用 GetReqHeader().Get(\"User-Agent\") ")
 
 		for k, v := range s.GetReqHeader() {
-			fmt.Print("        " + k)
+			fmt.Print("------------------          " + k)
 			fmt.Print(" : ")
 			fmt.Println(v)
 		}
-		fmt.Println("------------------  E Req Header")
+		fmt.Println("------------------------------------------------------")
 	} else {
-		fmt.Println("Req Header  ==> (" + s.GetReqHeader().Get(key) + ") End") // 例如 User-Agent
+		fmt.Println("------------------  Req Header  ==> (" + s.GetReqHeader().Get(key) + ") End") // 例如 User-Agent
 	}
 	fmt.Println("")
 }
 
 //打印 请求 Post内容
 func (s *Spider) PrintReqPostData() {
-	fmt.Println("------------------ Req PostData ==> ( " + s.reqPostData + " )")
+	fmt.Println("------------------  Req PostData ==> ( " + s.reqPostData + " )")
 	fmt.Println("")
 }
 
 //打印 响应 头信息
 func (s *Spider) PrintResHeader(key string) {
 	if key == "" {
-		fmt.Println("------------------  S Res 响应 Header")
+		fmt.Println("------------------  Res 响应 Header")
 		fmt.Println("------------------  使用 GetResHeader() http.Header 可以使用 GetResHeader().Get(\"Content-Encoding\") ")
 		for k, v := range s.GetResHeader() {
-			fmt.Print("        " + k)
+			fmt.Print("------------------          " + k)
 			fmt.Print(" : ")
 			fmt.Println(v)
 		}
-		fmt.Println("------------------  E Res Header")
+		fmt.Println("------------------------------------------------------")
 	} else {
-		fmt.Println("Res Header  ==> (" + s.GetResHeader().Get(key) + ") End") // 例如 Content-Encoding
+		fmt.Println("------------------  Res Header  ==> (" + s.GetResHeader().Get(key) + ") End") // 例如 Content-Encoding
 	}
 	fmt.Println("")
 }
@@ -51,9 +51,9 @@ func (s *Spider) PrintResSetCookie() {
 	fmt.Println("------------------  S Res 响应 Set-Cookie ")
 	fmt.Println("------------------  使用 GetResHeader()[\"Set-Cookie\"]  []string ")
 	for _, itemCookie := range s.GetResHeader()["Set-Cookie"] {
-		fmt.Println("        " + itemCookie)
+		fmt.Println("------------------          " + itemCookie)
 	}
-	fmt.Println("------------------  E Res 响应 Set-Cookie")
+	fmt.Println("------------------------------------------------------")
 	fmt.Println()
 }
 
@@ -68,7 +68,7 @@ func (s *Spider) PrintCookies(strUrl string) {
 	fmt.Println("------------------  S CookieJar  ==> From(" + strUrl + ")")
 	fmt.Println("------------------  使用 GetCookiesMap(strUrl string) map[string]string")
 	defer func() {
-		fmt.Println("------------------  E CookieJar")
+		fmt.Println("------------------------------------------------------")
 		fmt.Println("")
 	}()
 	if s.cookieJar == nil {
@@ -76,7 +76,7 @@ func (s *Spider) PrintCookies(strUrl string) {
 	}
 	mapCookieHost := s.GetCookiesMap(strUrl)
 	for k, v := range mapCookieHost {
-		fmt.Print("        " + k)
+		fmt.Print("------------------          " + k)
 		fmt.Print(" : ")
 		fmt.Println(v)
 	}
