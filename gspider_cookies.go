21ca4778cca42c1de8be2f95ea7b0245a7827900
ghostljj <ghostljj@gmail.com>
2019-03-27 02:34:36 +0800

    add getCookieJson

--- gspider_cookies.go
+++ gspider_cookies.go
@@ -1,11 +1,43 @@
 package gspider
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
+	"strconv"
 	"strings"
 )
+
+//一个全局方法 可以获取cookie json  可用于chrome的 EditThisCookie插件
+func GetCookieJson(strUrl, strCookie string) string {
+	URI, _ := url.Parse(strUrl)
+	var mapCookies []map[string]string
+	parts := strings.Split(strings.TrimSpace(strCookie), ";")
+	for i := 0; i < len(parts); i++ {
+		parts[i] = strings.TrimSpace(parts[i])
+		if len(parts[i]) == 0 {
+			continue
+		}
+		attr, val := parts[i], ""
+		if j := strings.Index(attr, "="); j >= 0 {
+			attr, val = attr[:j], attr[j+1:]
+		}
+		mapItem := make(map[string]string)
+		mapItem["domain"] = URI.Host
+		mapItem["name"] = attr
+		mapItem["path"] = URI.Path
+		if len(URI.Path) <= 0 {
+			mapItem["path"] = "/"
+		}
+		mapItem["value"] = val
+		mapItem["id"] = strconv.Itoa(i + 1)
+		mapCookies = append(mapCookies, mapItem)
+	}
+	byteCookies, _ := json.Marshal(mapCookies)
+	jsonCookies := string(byteCookies)
+	return jsonCookies
+}
 
 //获取Cookie
 func (s *Spider) Cookies(strUrl string) string {
